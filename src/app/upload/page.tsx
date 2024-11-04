b0d0059a0e19ddee5e8c00fcf0fdf80f11ce9ec9
DavidRicardoWilde <15224005+DavidRicardoWilde@users.noreply.github.com>
2024-11-04 21:33:40 +0800

    feat: upload file to blob

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import { useState } from 'react';
+import { type PutBlobResult } from '@vercel/blob';
+import {upload} from "@vercel/blob/client";
 
 export default function UploadPage() {
     const [selectedFile, setSelectedFile] = useState<File | null>(null);
@@ -8,6 +10,7 @@ export default function UploadPage() {
     const [title, setTitle] = useState<string>('');
     const [content, setContent] = useState<string>('');
     const [describe, setDescribe] = useState<string>('');
+    const [blob, setBlob] = useState<PutBlobResult | null>(null);
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const file = e.target.files?.[0];
@@ -28,8 +31,8 @@ export default function UploadPage() {
         const formData = new FormData();
         formData.append('file', selectedFile);
         formData.append('title', title);
-        formData.append('content', content);
-        formData.append('describe', describe);
+        // formData.append('content', content);
+        // formData.append('describe', describe);
 
         const token = localStorage.getItem('token');
         if (!token) {
@@ -37,18 +40,33 @@ export default function UploadPage() {
         }
 
         try {
-            const response = await fetch('https://wp-api.gluttongk.com/api/creator/uploadWork', {
-                method: 'POST',
-                body: formData,
-                headers: {
-                    'Authorization': `${token}`
-                }
-            });
+            // let fileUrl = '';
+            if (selectedFile.size > 4 * 1024 * 1024) {
+                console.log('uploading large file');
+                const newBlob = await upload(selectedFile.name, selectedFile, {
+                    access: 'public',
+                    handleUploadUrl: 'https://wp-api.gluttongk.com/api/creator/uploadWork',
+                    clientPayload: JSON.stringify({token: token, do_what: 'gen-code'}),
+                });
 
-            if (response.ok) {
-                alert('File uploaded successfully');
+                setBlob(newBlob);
             } else {
-                alert('File upload failed');
+                console.log('uploading small file');
+                // 使用现有的上传方式
+                // const response = await fetch('https://wp-api.gluttongk.com/api/creator/uploadWork', {
+                //     method: 'POST',
+                //     body: formData,
+                //     headers: {
+                //         'Authorization': `${token}`
+                //     }
+                // });
+                //
+                // if (response.ok) {
+                //     alert('File uploaded successfully');
+                // } else {
+                //     alert('File upload failed');
+                // }
+                return;
             }
         } catch (err) {
             alert('An error occurred. Please try again.');
@@ -81,20 +99,20 @@ export default function UploadPage() {
                             className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
                             required
                         />
-                        <textarea
-                            placeholder="Content"
-                            value={content}
-                            onChange={(e) => setContent(e.target.value)}
-                            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
-                            required
-                        />
-                        <textarea
-                            placeholder="Describe"
-                            value={describe}
-                            onChange={(e) => setDescribe(e.target.value)}
-                            className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
-                            required
-                        />
+                        {/*<textarea*/}
+                        {/*    placeholder="Content"*/}
+                        {/*    value={content}*/}
+                        {/*    onChange={(e) => setContent(e.target.value)}*/}
+                        {/*    className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"*/}
+                        {/*    required*/}
+                        {/*/>*/}
+                        {/*<textarea*/}
+                        {/*    placeholder="Describe"*/}
+                        {/*    value={describe}*/}
+                        {/*    onChange={(e) => setDescribe(e.target.value)}*/}
+                        {/*    className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"*/}
+                        {/*    required*/}
+                        {/*/>*/}
                         <div className="flex items-baseline justify-between">
                             <button className="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900">
                                 Upload
