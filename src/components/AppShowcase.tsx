f4650db910b73949017ee032488698f32794dbec
DavidRicardoWilde <15224005+DavidRicardoWilde@users.noreply.github.com>
2025-01-03 16:32:57 +0800

    add app show-case image

--- src/components/AppShowcase.tsx
+++ src/components/AppShowcase.tsx
@@ -8,7 +8,7 @@ export function AppShowcase() {
             </div>
             <div className="absolute top-0 bottom-0 left-0 right-0 overflow-hidden rounded-[2.5rem]">
                 <Image
-                    src="/app-screenshot.jpg"
+                    src="/images/app-showcase/home-index.png"
                     alt="WallpaperWizard App Interface"
                     layout="fill"
                     objectFit="cover"
