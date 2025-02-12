0a71c2079f1fa6eb748e7dcc5ce0c5416d76841d
DavidRicardoWilde <15224005+DavidRicardoWilde@users.noreply.github.com>
2025-02-12 15:46:44 +0800

    add eslint for nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,27 @@
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-  trailingSlash: true
+  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: false,
+    dirs: [
+      "app",
+      "components",
+      "config",
+      "contexts",
+      "i18n",
+      "lib",
+      "messages",
+      "middleware",
+      "models",
+      "service",
+      "type",
+      "utils",
+    ],
+  },
 };
 
 export default withNextIntl(nextConfig);
-
